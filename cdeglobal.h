9b4d77b6f2ef3e9b78e187cc92d9768c5dae5be4
hezex <89002817+hezex@users.noreply.github.com>
2022-07-09 17:13:23 +0800

    Include this file

---
+++ cdeglobal.h
@@ -0,0 +1,20 @@
+/**
+ * This file should be included
+ * CDE has opened source on Github!
+ * Get the latest version from https://github.com/hezex/Condows-Class-Directly-Encapsulates
+ * Copyright by hezex. Reach me at hezexi520@163.com
+*/
+
+#ifndef _CDE__
+#define _CDE__
+
+#define _CDE_VERSION__ 0x00
+
+#include "cdebasic.hpp"
+
+#undef _CDE__
+
+#else
+#error Class Directly Encapsulates has conflicted
+#endif
+
